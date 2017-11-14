2ae06d4e764c23b877712357b06310c58ac27ad4
wsdxl <506615839@qq.com>
2017-11-14 17:23:06 +0800

    增加了回帖方法，但回复过的帖子再次回帖会报错-20171114

--- features/reply.js
+++ features/reply.js
@@ -4,25 +4,28 @@ defineSupportCode(function ({ Given, When, Then }) {
     When('导航到回帖页面', function () {
         return this.driver.findElement({ css: '#topic_list > div:nth-child(1) > div > a' }).click();
     });
-    Then('点击照片输入路径{string} ，输入内容{string}', async function (string, string2) {
-        this.driver.findElement({ css: '.eicon-image' }).click();
-        this.driver.findElement({ name: 'file' }).sendKeys(string);
-        this.driver.sleep(2 * 1000);
+    
+    Then('点击照片输入路径{string},输入内容{string},点击回帖按钮，成功或者失败{string},回帖成功，页面含有{string}回帖失败，得到错误提示{string}', async function (string, string2,
+        string3, string4, string5) {
+        if (string !== "null") {
+            this.driver.findElement({ css: '.eicon-image' }).click();
+            this.driver.findElement({ name: 'file' }).sendKeys(string);
+            this.driver.sleep(2 * 1000);
+        }
         this.driver.findElement({ css: '.CodeMirror-scroll' }).click();
         let text = await this.driver.findElement({ css: 'div.CodeMirror-scroll > div:nth-child(2)' });
-        return this.driver.actions().mouseMove(text).sendKeys(string2).perform();
+        this.driver.actions().mouseMove(text).sendKeys(string2).perform();
+        return this.driver.findElement({ css: '.submit_btn' }).click();
+        if (string3 == "success") {
+            let text2 = await this.driver.findElement({ xpath: '//*[@id="content"]/div[2]/div[1] ' }).getText();
+            console.log(text2);
+            return assert.deepEqual(text2, string4);
+        } else {
+            let text3 = await this.driver.findElement({ css: 'strong' }).getText();
+            return assert.deepEqual(text3, string5);
+        }
 
-    });
-    Then('直接点击回帖按钮，得到提示信息{string}', async function (string) {
-        this.driver.findElement({ css: '.submit_btn' }).click();
-        let text3 = await this.driver.findElement({ css: 'strong' }).getText();
-        return assert.deepEqual(text3, string);
-    });
-    Then('回帖成功，跳转的页面含有{string}', async function (string) {
-        this.driver.findElement({ css: '.submit_btn' }).click();
-        let text2 = await this.driver.findElement({ xpath: '//*[@id="content"]/div[2]/div[1] ' }).getText();
-        console.log(text2);
-        return assert.deepEqual(text2, string);
+
     });
 
 })
